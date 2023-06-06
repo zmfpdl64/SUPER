7a31bf752b0b4612184ae4ac7155e1c404cb640d
leewoojin <zmfpdl64@naver.com>
2023-06-06 17:14:51 +0900

    feature: 5개 단어를 입력시 결과 및 css 변경

--- wordleJS/README.md
+++ wordleJS/README.md
@@ -7,5 +7,12 @@ flex-grow: 2;
 ```
 
 ```javascript
-const thisBlock = document.querySelector(".board-block[data-index]");
+const thisBlock = document.querySelector(`.board-block="$[data-index]"`);
 ```
+
+class명과 필드를 이용해서 태그를 가져올 수 있다.
+
+- [ ] 5개 단어를 입력하고 Enter를 쳤을 때
+  - [x] 알파뱃 저장
+  - [ ] 해당 알파뱃 가질 때
+  - [ ] 알파뱃 자리 일치할 때
--- wordleJS/app.js
+++ wordleJS/app.js
@@ -1,7 +1,19 @@
 let total = 0;
 let index = 0;
+const answer = "ABCDE";
+
 const back = "BACKSPACE";
 const enter = "ENTER";
+let str = "";
+
+const notExist = 0;
+const contain = 1;
+const correct = 2;
+
+const notExistColor = "#3A3A3C";
+const containColor = "#b59f3b";
+const correctColor = "#538D4E";
+
 const alphaMax = 90;
 const alphaMin = 65;
 const colMax = 5;
@@ -13,7 +25,9 @@ function inputAlpha(key, code, cur_idx) {
   if (alphaMin <= code && code <= alphaMax && index < colMax) {
     const domBlock = selectDom(cur_idx);
     domBlock.innerText = key;
+    str += key;
     index++;
+    console.log(str);
     return true;
   }
   return false;
@@ -27,28 +41,72 @@ function isEnter(key) {
   return false;
 }
 
-function isDelete(key) {
+function isDelete(key, cur_idx) {
   if (key === "BACKSPACE" && index > 0) {
+    str = str.slice(0, -1);
     selectDom(cur_idx - 1).innerText = "";
     index -= 1;
     return true;
   }
   return false;
 }
+function returnResult(inputs) {
+  const result = [];
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i] === answer[i]) {
+      result.push(correct);
+    } else if (answer.includes(inputs[i])) {
+      result.push(contain);
+    } else {
+      result.push(notExist);
+    }
+  }
+  str = "";
+  console.log(result);
+  return result;
+}
 
+/**
+ * const notExist = 0;
+ * const contain = 1;
+ * const correct = 2;
+ */
+
+function changeCss(result) {
+  let idx = total - result.length;
+  for (let i = 0; i < result.length; i++) {
+    const result_v = result[i];
+    const dom = selectDom(idx);
+    console.log(dom);
+    if (result_v === contain) {
+      dom.style.background = containColor;
+    } else if (result_v === correct) {
+      dom.style.background = correctColor;
+    } else {
+      dom.style.background = notExistColor;
+    }
+    idx++;
+  }
+}
+/**
+ * - [x] 해당 알파뱃 가질 때
+ * - [x] 알파뱃 자리 일치할 때
+ * - [ ] 결과를 확인하고 해당 css 변경
+ */
 const app = () => {
   const handleKeydown = (e) => {
     const key = e.key.toUpperCase();
     const code = e.keyCode;
     let cur_idx = total + index;
-
     if (inputAlpha(key, code, cur_idx)) {
       return;
     }
     if (isEnter(key)) {
+      const result = returnResult(str);
+      changeCss(result);
       return;
     }
-    if (isDelete(key)) {
+    if (isDelete(key, cur_idx)) {
       return;
     }
   };
