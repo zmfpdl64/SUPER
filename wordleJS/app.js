835d901042eebae3266c78decd36c32e2267de21
leewoojin <zmfpdl64@naver.com>
2023-06-06 15:50:08 +0900

    refactor: 모듈 단위로 쪼갬

--- wordleJS/app.js
+++ wordleJS/app.js
@@ -1,32 +1,55 @@
-const rows = {};
 let total = 0;
 let index = 0;
+const back = "BACKSPACE";
+const enter = "ENTER";
+const alphaMax = 90;
+const alphaMin = 65;
+const colMax = 5;
+
 function selectDom(idx) {
   return document.querySelector(`.board-block[data-index='${idx}']`);
+}
+function inputAlpha(key, code, cur_idx) {
+  if (alphaMin <= code && code <= alphaMax && index < colMax) {
+    const domBlock = selectDom(cur_idx);
+    domBlock.innerText = key;
+    index++;
+    return true;
+  }
+  return false;
+}
+function isEnter(key) {
+  if (key === "ENTER" && index === colMax) {
+    total += index;
+    index = 0;
+    return true;
+  }
+  return false;
+}
+
+function isDelete(key) {
+  if (key === "BACKSPACE" && index > 0) {
+    selectDom(cur_idx - 1).innerText = "";
+    index -= 1;
+    return true;
+  }
+  return false;
 }
 
 const app = () => {
   const handleKeydown = (e) => {
     const key = e.key.toUpperCase();
     const code = e.keyCode;
-    let sum = total + index;
-    const domBlock = selectDom(sum);
-    /**
-     * 1. 문자열 입력받기
-     */
-    if (65 <= code && code <= 90 && index < 5) {
-      domBlock.innerText = key;
-      index++;
+    let cur_idx = total + index;
+
+    if (inputAlpha(key, code, cur_idx)) {
       return;
     }
-    if (key === "ENTER" && index === 5) {
-      total += index;
-      index = 0;
+    if (isEnter(key)) {
       return;
     }
-    if (key === "BACKSPACE" && index > 0) {
-      selectDom(sum - 1).innerText = "";
-      index -= 1;
+    if (isDelete(key)) {
+      return;
     }
   };
   window.addEventListener("keydown", handleKeydown);
