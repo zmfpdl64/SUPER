64b00d929a57220f0abd14da6080d543d90988a2
leewoojin <zmfpdl64@naver.com>
2023-06-21 13:56:13 +0900

    백 복구

--- wordlePy/main.py
+++ wordlePy/main.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import random
+
 app = FastAPI()
+
+app.mount("/wordle", StaticFiles(directory="../wordleJS", html=True), name="wordle")
+app.mount("/carret", StaticFiles(directory="../carretUI", html=True), name="carret")
+
+
+# arr = ["RAMEN","APPLE", "COLOR", "BACKE", "FRONT"]
+arr = ["CRAZY"]
+
+@app.get("/answer")
+def returnAnswer():
+    idx = random.randrange(0, len(arr))
+    return arr[idx]
